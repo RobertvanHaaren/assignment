399fbf76318f239b751f35f57ab9bc5c295055ed
Robert van Haaren <robertvanhaaren@hotmail.nl>
2022-07-30 10:12:57 +0200

    Adds navigation bar to pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,34 @@ import Server from "./pages/pricing/components/ServerPricing";
 
 function App() {
   return (
-    <div style={{ margin: "50px" }}>
-      <h1>Pricing Page</h1>
+    <div>
+
+      <div className="navbar">
+        <div className="navbar__wrapper flex">
+          <div className="navbar__logo">
+            <img src="ocotopusdeploy-logo.svg" alt="Octopus Deploy Logo" />
+          </div>
+          <nav className="navbar__menu">
+            <ul className="navbar__menu-items">
+              <li className="navbar__menu-li"><a href="#">Features</a></li>
+              <li className="navbar__menu-li"><a href="#">Pricing</a></li>
+              <li className="navbar__menu-li"><a href="#">What's New</a></li>
+              <li className="navbar__menu-li"><a href="#">Recources</a></li>
+              <li className="navbar__menu-li"><a href="#">Blog</a></li>
+              <li className="navbar__menu-li"><a href="#">Enterprise</a></li>
+              <li className="navbar__menu-li"><a href="#">Sign-in</a></li>
+              <li className="navbar__menu-li"><button className="btn">Start a trial {'>'}</button></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <h1>One product, two ways to host it</h1>
       <div>
         <Cloud />
         <Server />
       </div>
+
     </div>
   );
 }
